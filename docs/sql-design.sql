92cbdecccca71d70367f86486d5535f965d91f0d
WooGeun-Nam <rolru7686@gmail.com>
2023-01-26 10:11:43 +0900

    model update

--- docs/sql-design.sql
+++ docs/sql-design.sql
@@ -16,7 +16,7 @@ select * from board;
 
 select * from user;
 
-select b.no, b.title, u.name, b.hit, date_format(b.reg_date, '%Y-%m-%d %h:%i:%s'), b.depth, u.no from board b join user u on b.user_no = u.no order by b.g_no desc, b.o_no asc limit 0,5;
+select b.no, b.title, u.name, b.hit, date_format(b.reg_date, '%Y-%m-%d %h:%i:%s'), b.depth, u.no from board b join user u on b.user_no = u.no where title like '%%' order by b.g_no desc, b.o_no asc limit 0,5 ;
 
 insert into board values(null, '바빠요', '바쁩니다링', 0, now(), 1, 1, 0, 3);
 insert into board values(null, '바빠요', '바쁩니다링', 0, now(), 1, 2, 1, 4);
